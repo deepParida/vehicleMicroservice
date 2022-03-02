a3eff75ce710de8037157c5da8a7d17aa4294295
DEEPTI RANJAN <DEEPTI RANJAN@DESKTOP-DFLUT55.bbrouter>
2022-03-03 00:17:03 +0530

    email

--- email-notifer/src/main/java/com/vehicle/tracker/email/notifer/EmailNotiferApplication.java
+++ email-notifer/src/main/java/com/vehicle/tracker/email/notifer/EmailNotiferApplication.java
@@ -1,6 +1,7 @@
 package com.vehicle.tracker.email.notifer;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
